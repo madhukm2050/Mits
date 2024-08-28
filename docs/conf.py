4c68f124edc7a789914cffa1c427e446b25f4f07
Praneeth <praneethvakkalagadda0430@gmail.com>
2024-08-28 22:46:40 +0530

    Documentation

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0,os.path.abspath('..'))
+scripts=r'C:\Users\prane\PycharmProjects'
+if scripts not in sys.path:
+    sys.path.append(scripts)
+
+project = 'Mits_AI_Dreamers'
+copyright = '2024, Madhu Praneeth Lokesh'
+author = 'Madhu Praneeth Lokesh'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.todo','sphinx.ext.viewcode','sphinx.ext.autodoc']
+
+todo_include_todos = True
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
