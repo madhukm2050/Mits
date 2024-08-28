b193cbbbe78e34d720b98973a3c39fe7f8e54a7e
Praneeth <praneethvakkalagadda0430@gmail.com>
2024-08-28 21:51:39 +0530

    Doc strings updated

--- Mits_AI_Dreamers/Similarity/jaccardcoefficient.py
+++ Mits_AI_Dreamers/Similarity/jaccardcoefficient.py
@@ -1,9 +1,48 @@
 class JaccardCoefficient:
+    """
+        A class used to represent and compute the Jaccard Coefficient between two strings.
+
+        Attributes
+        ----------
+        string1 : set
+            The set of unique characters from the first string.
+        string2 : set
+            The set of unique characters from the second string.
+
+        Methods
+        -------
+        findJaccardCoefficient():
+            Computes and returns the Jaccard Coefficient between the two sets of characters.
+        """
     def __init__(self, string1, string2):
+        """
+                Initializes the JaccardCoefficient class with two strings.
+
+                The strings are converted to sets of unique characters.
+
+                Parameters
+                ----------
+                string1 : str
+                    The first string for comparison.
+                string2 : str
+                    The second string for comparison.
+                """
         self.string1 = set(string1)
         self.string2 = set(string2)
 
     def findJaccardCoefficient(self):
+        """
+        Computes the Jaccard Coefficient between the two sets of characters.
+
+        The Jaccard Coefficient is defined as 1 minus the Jaccard Index, where
+        the Jaccard Index is the size of the intersection divided by the size of the union
+        of the two sets.
+
+        Returns
+        -------
+        float
+           The Jaccard Coefficient between the two sets of characters.
+        """
         intersection = self.string1.intersection(self.string2)
         union = self.string1.union(self.string2)
 
--- Mits_AI_Dreamers/Similarity/overlapcofficient.py
+++ Mits_AI_Dreamers/Similarity/overlapcofficient.py
@@ -1,10 +1,53 @@
 class overlapCoefficient:
 
+    """
+    A class used to represent and compute the Overlap Coefficient between two strings.
+
+    Attributes
+    ----------
+    string1 : set
+        The set of unique characters from the first string.
+    string2 : set
+        The set of unique characters from the second string.
+
+    Methods
+    -------
+    findOverlapCoefficient():
+        Computes and returns the Overlap Coefficient between the two sets of characters.
+    """
+
     def __init__(self, string1, string2):
+
+        """
+        Initializes the OverlapCoefficient class with two strings.
+
+        The strings are converted to sets of unique characters.
+
+        Parameters
+        ----------
+        string1 : str
+            The first string for comparison.
+        string2 : str
+            The second string for comparison.
+        """
+
         self.string1 = set(string1)
         self.string2 = set(string2)
 
     def findOverlapCoefficient(self):
+
+        """
+        Computes the Overlap Coefficient between the two sets of characters.
+
+        The Overlap Coefficient is defined as the size of the intersection divided
+        by the size of the smaller set of the two. The coefficient is then subtracted
+        from 1 to obtain the final result.
+
+        Returns
+        -------
+        float
+            The Overlap Coefficient between the two sets of characters.
+        """
 
         intersection = self.string1.intersection(self.string2)
 
