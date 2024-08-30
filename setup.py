669aac381ec29512864d15e5418e98ed58309512
Lokesh <lokeshreddy2680@gmail.com>
2024-08-30 22:27:45 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open('README.md', 'r') as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
@@ -8,18 +8,17 @@ setuptools.setup(
     version='2024.0.1',
     author='Madhu Praneeth Lokesh',
     author_email='lokeshreddy2680@gmail.com',
-    description='This is Hamming distance, Jaccard coefficient, overlap',
+    description='A package for computing Hamming distance, Jaccard coefficient, and overlap.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(),  # Ensures it finds all sub-packages
     url='https://github.com/madhukm2050/Mits_AI_Dreamers',
-    license='',
+    license='GPLv3',
     install_requires=[
         'psutil',
         'pandas',
         'plotly',
         'matplotlib',
-        'resource',
         'validators',
         'urllib3',
         'Pillow',
@@ -31,16 +30,22 @@ setuptools.setup(
         'deprecated',
     ],
     extras_require={
-        'gpu':  ['cupy', 'pycuda'],
+        'gpu': ['cupy', 'pycuda'],
         'spark': ['pyspark'],
         'dev': ['twine', 'setuptools', 'build'],
         'all': ['cupy', 'pycuda', 'pyspark', 'twine', 'setuptools', 'build']
     },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.5',
+    python_requires='>=3.6',
 )
